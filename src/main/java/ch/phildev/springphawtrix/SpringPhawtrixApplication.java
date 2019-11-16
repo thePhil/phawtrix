da55970fd45e026d01e6c483a5f801896c7140d3
Philipp Tölle <philipptoelle@gmail.com>
2019-11-16 10:10:02 +0100

    Initial commit

---
+++ src/main/java/ch/phildev/springphawtrix/SpringPhawtrixApplication.java
@@ -0,0 +1,13 @@
+package ch.phildev.springphawtrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPhawtrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPhawtrixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/phildev/springphawtrix/SpringPhawtrixApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.phildev.springphawtrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPhawtrixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
